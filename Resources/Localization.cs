e86efbf393c74cb38e1758e9457c093faee4452c
anvslv <anvslv@gmail.com>
2014-01-10 03:10:31 +0400

    Moved to inline localization

--- Resources/Localization.cs
+++ Resources/Localization.cs
@@ -1,4 +1,9 @@
+using System.Collections;
+using System.Globalization;
+using System.Resources;
 using System.Threading;
+using System.Web;
+using Microsoft.Ajax.Utilities;
 
 namespace Blog.Resources
 {
@@ -7,6 +12,40 @@ namespace Blog.Resources
         public static string CurrentLanguage
         {
             get { return Thread.CurrentThread.CurrentCulture.Name; }
+        }
+
+        public static string GetLocalResources()
+        {
+            ResourceSet resourceSetAdmin = Index_Admin.ResourceManager.GetResourceSet
+                (CultureInfo.CurrentUICulture, true, true);
+            var content = string.Empty;
+
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                content = "var Resources = {};\n";
+                foreach (DictionaryEntry res in resourceSetAdmin)
+                {
+                    content += string.Format("Resources.{0} = '{1}';\n", res.Key,
+                        HttpUtility.JavaScriptStringEncode(res.Value.ToString()));
+                }
+            }
+
+            if (HttpContext.Current.IsDebuggingEnabled)
+            {
+                return content;
+            }
+            else
+            {
+                return Minify(content);
+            }
+        }
+
+        private static string Minify(string content)
+        {
+            Minifier minifier = new Minifier();
+
+            CodeSettings settings = new CodeSettings { PreserveImportantComments = false };
+            return minifier.MinifyJavaScript(content, settings);
         }
 
         public static string[] Resources
