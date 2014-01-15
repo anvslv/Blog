60516d728c5d500493a2216dca1444c641f0dbaf
anvslv <anvslv@gmail.com>
2014-01-15 07:43:04 +0400

    Made login use ajax

---
+++ App/Handlers/LoginHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Security;
+using Blog.App.Code;
+using BlogExtensions.Resources;
+using Microsoft.Ajax.Utilities;
+
+namespace Blog.App.Handlers
+{
+    public class LoginHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            if (context.Request.HttpMethod == "POST")
+            {
+                string username = context.Request.Form["username"];
+                string password = context.Request.Form["password"];
+                string remember = context.Request.Form["remember"];
+
+                if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+                {
+                    if (FormsAuthentication.Authenticate(username, password))
+                    {
+                        FormsAuthentication.SetAuthCookie(username, remember == "true");
+                        context.Response.StatusCode = 200;
+                    } 
+                    else
+                    {
+                        context.Response.StatusCode = 401;
+                        context.Response.SuppressFormsAuthenticationRedirect = true;
+                        context.Response.End();
+                    }
+                }
+                else if (!string.IsNullOrEmpty(context.Request.QueryString["signout"]))
+                {
+                    FormsAuthentication.SignOut();
+                    context.Response.Redirect(context.Request.QueryString["ReturnUrl"], true);
+                }
+            }  
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+    }
+} 
--- scripts/application.js
+++ scripts/application.js
@@ -37,9 +37,67 @@ App.changeLocalization = function() {
     });
 };
 
+App.showLoginForm = function() {
+    $('.popup-with-form').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#username',
+
+        // When elemened is focused, some mobile browsers in some cases zoom in
+        // It looks not nice, so we disable it:
+        callbacks: {
+            beforeOpen: function() {
+                if ($(window).width() < 700) {
+                    this.st.focus = false;
+                } else {
+                    this.st.focus = '#name';
+                }
+            }
+        }
+    });
+
+    jQuery.fn.shake = function() {
+        this.each(function(i) {
+            $(this).css({ "position": "relative" });
+            for (var x = 1; x <= 2; x++) {
+                $(this).animate({ left: -25 }, 10).animate({ left: 0 }, 50).animate({ left: 25 }, 10).animate({ left: 0 }, 50);
+            }
+        });
+        return this;
+    };
+
+    $("#login").submit(function (e) {
+
+        var url = $("#login").attr('action');
+        var username = $("#username").val();
+        var password = $("#password").val();
+        var remember = $("#remember").checked == true;
+
+        $.ajax({
+            url: url,
+            data: { username: username, password: password, remember: remember },
+            type: 'POST',
+            dataType: "html",
+            statusCode: {
+                200: function() {
+                    location.reload();
+                    return true;
+                },
+                401: function() {
+                    //$("#loginIncorrect").removeClass("hidden");
+                    $('.white-popup-block').shake();
+                    return false;
+                }
+            }
+        });
+        return false;
+    });
+};
+
 $(function () { 
     App.applyMagnificPopup();
     App.applyHighlightJs();
     App.changeLocalization();
+    App.showLoginForm();
 });
  
